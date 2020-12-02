ae31e788d57106779a1aecae35f84cd7ee1965db
Jacky Nguyen <48352366+JackyTNguyen@users.noreply.github.com>
2020-12-02 12:39:58 +0100

    new files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-new Glide('.glide').mount()
+var glide = new Glide('.glide', {
+    type: 'carousel',
+    focusAt: 'center',
+    perView: 1,
+    animationDuration: 1000,
+    autoplay: 5000,
+    hoverpause: true,
+    breakpoints: {
+      1200: {
+        perView: 1
+      },
+      800: {
+        perView: 1
+      },
+      480: {
+        perView: 1
+      }
+    }
+  })
+  
+  glide.mount()
